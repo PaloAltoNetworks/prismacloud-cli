9bf26e16bd8d82e1831268eb33be9e59a3d99f06
Steven de Boer <stdeboer@paloaltonetworks.com>
2022-04-13 18:07:49 +0200

    Change import order

--- prismacloud/cli/__init__.py
+++ prismacloud/cli/__init__.py
@@ -9,14 +9,11 @@ import click
 import click_completion
 import coloredlogs
 import pandas as pd
-
+import prismacloud.cli.version as cli_version
+from click_help_colors import HelpColorsMultiCommand
 from pydantic import BaseSettings
 from tabulate import tabulate
 from update_checker import UpdateChecker
-from click_help_colors import HelpColorsMultiCommand
-
-import prismacloud.cli.version as cli_version
-
 
 click_completion.init()
 
