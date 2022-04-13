7d084d2f72a208a0867ecd9139723e2a0c60365b
Steven de Boer <stdeboer@paloaltonetworks.com>
2022-04-13 09:10:21 +0200

    Use max_columns in html output

--- prismacloud/cli/__init__.py
+++ prismacloud/cli/__init__.py
@@ -215,7 +215,7 @@ def cli_output(data, sort_values=False):
 </head>
                 """  # noqa
             click.secho(pre_table_html)
-            click.secho(data_frame.to_html(classes="table table-sm table-striped text-center", justify="center"), fg="green")
+            click.secho(data_frame.to_html(max_cols=settings.max_columns, classes="table table-sm table-striped text-center", justify="center"), fg="green")
             # post-table-html
             post_table_html = """
 
