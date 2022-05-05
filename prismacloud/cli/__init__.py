5f518d27f715cfeb1d9108a2d4240131b3c6fbe9
Steven de Boer <stdeboer@paloaltonetworks.com>
2022-05-05 14:15:00 +0200

    Fixed bare except (Flake)

--- prismacloud/cli/__init__.py
+++ prismacloud/cli/__init__.py
@@ -238,8 +238,8 @@ def cli_output(data, sort_values=False):
             # Calculate a new column usage based as percentage on column used and column workloadsPurchased
             data_frame["usage"] = data_frame["used"] / data_frame["workloadsPurchased"] * 100
             # Extra columns are added, proceed.
-        except:
-            logging.debug("No dataPoints.counts or workloadsPurchased column")
+        except Exception as _exc:
+            logging.debug("No dataPoints.counts or workloadsPurchased column found")
 
         # Drop all rows after max_rows
         data_frame = data_frame.head(settings.max_rows)
