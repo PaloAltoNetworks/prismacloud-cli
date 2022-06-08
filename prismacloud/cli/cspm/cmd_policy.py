d6b5c6421697a4c6cf047d49d4dc5ad62a2d7984
Simon Melotte <smelotte@paloaltonetworks.com>
2022-06-08 13:46:35 +0200

    add blank lines

--- prismacloud/cli/cspm/cmd_policy.py
+++ prismacloud/cli/cspm/cmd_policy.py
@@ -17,6 +17,7 @@ def list_policies():
     result = pc_api.policy_list_read()
     cli_output(result)
 
+
 @click.command("set", short_help="[CSPM] Turn on and off policies")
 
 
@@ -51,6 +52,8 @@ def list_policies():
     type=click.Choice(["enable", "disable"]),
     help="Policy status to set (enable or disable)."
 )
+
+
 def enable_or_disable_policies(policy_severity, all_policies, cloud_type, policy_type, status, compliance_standard):
     """Enable or Disable policies"""
     specified_policy_status = bool(status.lower() == "enable")
