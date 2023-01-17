bdf82bb15450f8a6fe66ad480b3f5b45d39d42b3
Simon Melotte <smelotte@paloaltonetworks.com>
2023-01-17 18:15:50 +0100

    add feature to global search accross all repositories

--- prismacloud/cli/pccs/cmd_repositories.py
+++ prismacloud/cli/pccs/cmd_repositories.py
@@ -62,5 +62,39 @@ def repository_update(integration_type, integration_id, repositories, separator)
     logging.info("API - Repository has been updated: %s", result)
 
 
+@click.command("search", short_help="Search accross all repositories")
+@click.option(
+    "--integration_type",
+    default="github",
+    type=click.Choice(
+        ["Github", "Bitbucket", "Gitlab", "AzureRepos", "cli", "AWS", "Azure", "GCP", "Docker", "githubEnterprise", "gitlabEnterprise", "bitbucketEnterprise", "terraformCloud", "githubActions", "circleci", "codebuild", "jenkins", "tfcRunTasks", "admissionController", "terraformEnterprise"]
+    ),
+    help="Type of the integration to update",
+)
+def global_search(integration_type):
+    """Search accross all repositories"""
+    logging.info("API - Search accross all repositories ...")
+    repositories = pc_api.repositories_list_read(query_params={"errorsCount": "true"})
+    
+    for repository in repositories:      
+        if repository["source"] == integration_type:  
+            logging.info("ID for the repository %s, Name of the Repository to scan: %s, Type=%s, default branch=%s", repository["id"], repository["repository"], repository["source"], repository["defaultBranch"] )
+
+            body_params = []
+            parameters = {}
+            parameters["sourceTypes"] = [repository["source"]]
+            parameters["types"] = ["Errors"]
+            parameters["repository"] = repository["repository"]
+            # parameters["repositoryId"] = repository["id"]
+            # parameters["branch"] = repository["defaultBranch"]
+            parameters["search"] = "{\"text\":\"Entropy Strings\", \"options\":[\"path\",\"code\"], \"title\":\"descriptive_title\"}"
+            body_params.append(parameters)    
+            logging.info("API - body_params ======== %s", parameters)
+
+            impacted_repositories = pc_api.errors_files_list(criteria=body_params)
+            logging.info("Return %s", impacted_repositories)        
+
+
 cli.add_command(list_repositories)
 cli.add_command(repository_update)
+cli.add_command(global_search)
