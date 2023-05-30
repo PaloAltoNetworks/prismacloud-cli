b215830dc1a9f4cd525a85a66c7bec6ba0a84e53
Steven de Boer <stdeboer@paloaltonetworks.com>
2023-05-30 08:40:40 +0200

    First working version if incidents change

--- prismacloud/cli/cwpp/cmd_incidents.py
+++ prismacloud/cli/cwpp/cmd_incidents.py
@@ -1,5 +1,5 @@
 import click
-
+import logging
 from prismacloud.cli import cli_output, pass_environment
 from prismacloud.cli.api import pc_api
 
@@ -14,15 +14,50 @@ def cli(ctx):
 @click.option('--search', type=str, help="Search term for the results.")
 @click.option('--sort', type=str, help="Sort key for the results.")
 @click.option('--reverse', is_flag=True, help="Flag to sort the results in reverse order.")
-def list_incidents(offset, limit, search, sort, reverse):
+@click.option('--archived', is_flag=True, help="Flag to list archived incidents.")
+def list_incidents(offset, limit, search, sort, reverse, archived):
+    logging.debug("Preparing to retrieve incidents")
     query_params = {
         'offset': offset,
         'limit': limit,
         'search': search,
         'sort': sort,
-        'reverse': reverse
+        'reverse': reverse,
+        'archived': archived
     }
     result = pc_api.get_endpoint("audits/incidents", query_params=query_params)
+    logging.debug(f"Retrieved {len(result)} incidents")
     cli_output(result)
 
+def handle_incidents(id, category, type, operation):
+    logging.debug(f"{operation.capitalize()} incidents...")
+    if id:
+        pc_api.execute_compute('PATCH', f"api/v1/audits/incidents/acknowledge/{id}", body_params={"acknowledged": operation == 'archive'})
+    else:
+        # Get all incidents
+        incidents = pc_api.get_endpoint("audits/incidents")
+        for incident in incidents:
+            if category and incident['category'] != category:
+                continue
+            if type and incident['type'] != type:
+                continue
+            logging.debug(f"{operation.capitalize()} incident: {incident['_id']}")
+            pc_api.execute_compute('PATCH', f"api/v1/audits/incidents/acknowledge/{incident['_id']}", body_params={"acknowledged": operation == 'archive'})
+
+@click.command(name="archive")
+@click.option('--id', type=str, help="ID of the incident to acknowledge.")
+@click.option('--category', type=str, help="Category of incidents to acknowledge.")
+@click.option('--type', type=str, help="Type of incidents to acknowledge.")
+def archive_incidents(id, category, type):
+    handle_incidents(id, category, type, 'archive')
+
+@click.command(name="restore")
+@click.option('--id', type=str, help="ID of the incident to restore.")
+@click.option('--category', type=str, help="Category of incidents to restore.")
+@click.option('--type', type=str, help="Type of incidents to restore.")
+def restore_incidents(id, category, type):
+    handle_incidents(id, category, type, 'restore')
+
 cli.add_command(list_incidents)
+cli.add_command(archive_incidents)
+cli.add_command(restore_incidents)
