fdef4c882e958a797f90f8e6eb5ccb9b1f78aebf
Simon Melotte <smelotte@paloaltonetworks.com>
2023-08-17 16:32:56 +0200

    add compliance and vulnerability report for hosts

--- prismacloud/cli/cwpp/cmd_hosts.py
+++ prismacloud/cli/cwpp/cmd_hosts.py
@@ -1,4 +1,5 @@
 import click
+import re
 
 from prismacloud.cli import cli_output, pass_environment
 from prismacloud.cli.api import pc_api
@@ -10,13 +11,81 @@ def cli(ctx):
     pass
 
 
-@click.command()
+@click.command(name="report", help="Full report for each hosts")
 @click.option("--complianceids", "compliance_ids", help="Filter by compliance id.")
-def compliance(compliance_ids=""):
-    result = pc_api.get_endpoint(
-        "hosts", {"complianceIDs": compliance_ids, "sort": "comptrimlianceIssuesCount", "reverse": "true"}
-    )
+def report(compliance_ids=""):
+
+    query_param = {"sort": "complianceRiskScore", "reverse": "true"}
+    if compliance_ids:
+        query_param = {"complianceIDs": compliance_ids, "sort": "complianceRiskScore", "reverse": "true"}
+    result = pc_api.hosts_list_read(query_param)
     cli_output(result)
 
 
-cli.add_command(compliance)
+@click.command(name="compliances", help="Get a report for compliance issues for each hosts")
+def compliances():
+
+    query_param = {"sort": "complianceRiskScore", "reverse": "true"}
+    hosts = pc_api.hosts_list_read(query_param)
+
+    data = []
+
+    for host in hosts:
+        if not host['complianceIssues']:
+            continue
+        for issue in host['complianceIssues']:
+
+            # Extract compliance framework from title
+            match = re.search(r'\(([^)]+)\)', issue['title'])
+            if match:
+                compliance_framework = match.group(1)
+            else:
+                continue  # Skip this issue if no compliance framework found
+
+            data.append({
+                "hostname": host['hostname'],
+                "scanTime": host['scanTime'],
+                "complianceIssuesCount": host['complianceIssuesCount'],
+                "complianceRiskScore": host['complianceRiskScore'],
+                "compliance_framework": compliance_framework,
+                "id": issue['id'],
+                "severity": issue['severity'],
+                "cause": issue['cause'],
+                "description": issue['description'],
+                "title": issue['title'],
+            })
+
+    cli_output(data)
+
+
+@click.command(name="vulnerabilities", help="Get a report for vulnerability issues for each hosts")
+def vulnerabilities():
+
+    query_param = {"sort": "vulnerabilityRiskScore", "reverse": "true"}
+    hosts = pc_api.hosts_list_read(query_param)
+
+    data = []
+
+    for host in hosts:
+        if not host['vulnerabilities']:
+            continue
+        for issue in host['vulnerabilities']:
+            data.append({
+                "hostname": host['hostname'],
+                "scanTime": host['scanTime'],
+                "vulnerabilitiesCount": host['vulnerabilitiesCount'],
+                "vulnerabilityRiskScore": host['vulnerabilityRiskScore'],
+                "cve": issue['cve'],
+                "severity": issue['severity'],
+                "cvss": issue['cvss'],
+                "packageName": issue['packageName'],
+                "packageVersion": issue['packageVersion'],
+                "status": issue['status'],
+            })
+
+    cli_output(data)
+
+
+cli.add_command(report)
+cli.add_command(compliances)
+cli.add_command(vulnerabilities)
