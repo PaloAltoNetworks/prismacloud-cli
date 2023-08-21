3960347f772b1d844140678c94dc39b3d3155f73
Steven de Boer <stdeboer@paloaltonetworks.com>
2023-08-21 08:51:40 +0200

    Fixed flake issues

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,6 @@
-import pytest
 import subprocess
 import os
+import pytest
 
 commands = [
     ["-o", "csv", "policy"],
@@ -14,6 +14,7 @@ commands = [
     ["--columns", "defendersSummary.host", "stats", "dashboard"]
 ]
 
+
 @pytest.mark.parametrize("command", commands, ids=[str(command) for command in commands])
 def test_cli_commands(command, benchmark):
     """Test various CLI commands and check if they run successfully."""
@@ -22,8 +23,11 @@ def test_cli_commands(command, benchmark):
         try:
             result = subprocess.run(["python3", "bin/pc"] + command, capture_output=True, text=True, check=True)
             assert result.returncode == 0
-        except subprocess.CalledProcessError as e:
-            pytest.fail(f"Command {' '.join(command)} failed with return code {e.returncode} and output:\\n{e.output}")
+        except subprocess.CalledProcessError as test_error:
+            pytest.fail(
+                f"Command {' '.join(command)} failed with return code {test_error.returncode} "
+                f"and output:\\n{test_error.output}"
+            )
 
     if os.environ.get("SKIP_BENCHMARK") == "1":
         run_command()
