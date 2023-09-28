b7aff70603de5cd9afa5e05a03f8257d653831b1
jpummill <jpummill@yahoo.com>
2023-09-28 14:29:14 -0500

    Updated prismacloud/cli/__init__.py, changed bootstrap to current (5.3.2)

--- prismacloud/cli/__init__.py
+++ prismacloud/cli/__init__.py
@@ -415,8 +415,10 @@ def show_output(data_frame, params, data):
 
 <html>
 <head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 </head>
+<body>
                 """  # noqa
             click.secho(pre_table_html)
             click.secho(
@@ -432,7 +434,8 @@ def show_output(data_frame, params, data):
             # post-table-html
             post_table_html = """
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
 </html>
 
                 """  # noqa
