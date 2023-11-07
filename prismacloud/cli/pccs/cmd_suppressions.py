6699ae498ffc714f1693a1f95de70c1760396713
Simon Melotte <smelotte@paloaltonetworks.com>
2023-11-07 12:01:10 +0100

    fix flake8

--- prismacloud/cli/pccs/cmd_suppressions.py
+++ prismacloud/cli/pccs/cmd_suppressions.py
@@ -85,65 +85,62 @@ def list_justifications():
             "terraformEnterprise",
         ]
     ),
-    required=True, 
+    required=True,
     help="Type of the integration to update",
 )
-@click.option("-r","--repository", required=True, help="Repository Name. e.g.: 'SimOnPanw/my-terragoat'")
-@click.option("-f","--files", multiple=True, help="File Name. Can specify multiple. e.g.: '-f s3.tf -f sns.tf'")
-def create(integration_type, repository, files):    
+@click.option("-r", "--repository", required=True, help="Repository Name. e.g.: 'SimOnPanw/my-terragoat'")
+@click.option("-f", "--files", multiple=True, help="File Name. Can specify multiple. e.g.: '-f s3.tf -f sns.tf'")
+def create(integration_type, repository, files):
     """Create new suppression"""
     data = []
     current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
     parameters = {}
-    parameters["sourceTypes"] = [integration_type]  
+    parameters["sourceTypes"] = [integration_type]
     parameters["repository"] = repository
-    
-    ## Get all the files that contain errors in them
-    all_repo_file_error_summaries = pc_api.errors_files_list(criteria = parameters)['data']
+
+    # Get all the files that contain errors in them
+    all_repo_file_error_summaries = pc_api.errors_files_list(criteria=parameters)["data"]
 
     for file_summary in all_repo_file_error_summaries:
-        # Process all files if no specific files are provided, 
+        # Process all files if no specific files are provided,
         # or just the specific ones if they are provided and match the current file path
-        if not files or any(file_summary['filePath'].endswith(f) for f in files):
+        if not files or any(file_summary["filePath"].endswith(f) for f in files):
             if files:  # This check ensures we only log for specific files, not all files
                 logging.info(f"Parsing this specific files: {file_summary['filePath']}")
 
-            parameters['filePath'] = file_summary['filePath']
+            parameters["filePath"] = file_summary["filePath"]
             parameters["types"] = ["Errors"]
 
             impacted_files = pc_api.errors_file_list(criteria=parameters)
-            for error_in_file in  impacted_files:
+            for error_in_file in impacted_files:
                 resource_id = f"{error_in_file['errorId']}::{repository}::{error_in_file['resourceId']}"
-                body_data = {  
-                    'comment': f'{current_date} - Suppressed via Prisma Cloud CLI.',  
-                    'origin': 'Platform', 
-                    'resources': { 
-                        'id': resource_id,    
-                        'accountId': repository
-                        },  
-                    'suppressionType': 'Resources'
-                }    
-                try:                            
-                    pc_api.suppressions_create(error_in_file['errorId'], body_data)                    
+                body_data = {
+                    "comment": f"{current_date} - Suppressed via Prisma Cloud CLI.",
+                    "origin": "Platform",
+                    "resources": {"id": resource_id, "accountId": repository},
+                    "suppressionType": "Resources",
+                }
+                try:
+                    pc_api.suppressions_create(error_in_file["errorId"], body_data)
                     data = data + [
                         {
-                            "action": 'Suppresed by Policy',
-                            "policy": error_in_file['errorId'],
+                            "action": "Suppresed by Policy",
+                            "policy": error_in_file["errorId"],
                             "repository": repository,
-                            "file": error_in_file['resourceId'],
-                            "comment": f'{current_date} - Suppressed via Prisma Cloud CLI.',
+                            "file": error_in_file["resourceId"],
+                            "comment": f"{current_date} - Suppressed via Prisma Cloud CLI.",
                         }
                     ]
-                    logging.info(f"Suppression created for {error_in_file['resourceId']} in repository {repository}")                
+                    logging.info(f"Suppression created for {error_in_file['resourceId']} in repository {repository}")
                 except Exception as e:
                     logging.error(f"An error occurred while creating suppression: {e}")
                     data = data + [
                         {
-                            "action": 'Error during suppression',
-                            "policy": error_in_file['errorId'],
+                            "action": "Error during suppression",
+                            "policy": error_in_file["errorId"],
                             "repository": repository,
-                            "file": error_in_file['resourceId'],
-                            "comment": f'{current_date} - Suppressed via Prisma Cloud CLI.',
+                            "file": error_in_file["resourceId"],
+                            "comment": f"{current_date} - Suppressed via Prisma Cloud CLI.",
                         }
                     ]
 
